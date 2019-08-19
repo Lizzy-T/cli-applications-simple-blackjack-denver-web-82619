d5870a90de11eebbabf7a96cec2d5fc9cf47f51e
Lizzy Tong <lizzy.tong33@gmail.com>
2019-08-19 19:59:25 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -57,7 +57,7 @@ def runner
   welcome
   card_total = initial_round
   display_card_total(card_total)
-  until card_total > 21 do 
+  until card_total > 21 do
     hit?(card_total)
     exit
   end
